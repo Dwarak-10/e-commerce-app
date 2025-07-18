80e386c79756e0231572e621a39ac120536765cf
Dwarak-10 <dwarakeshvaran0512@gmail.com>
2025-07-18 17:14:37 +0530

    msg

--- e-commerce/src/components/LoginForm.jsx
+++ e-commerce/src/components/LoginForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
+import * as Yup from "yup"
 
 const validationSchema = (formName) => Yup.object({
     username: Yup.string().when([], {
