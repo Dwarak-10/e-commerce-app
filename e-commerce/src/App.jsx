45089ec81515a133878801b1b401b535d4e066ed
Dwarak-10 <dwarakeshvaran0512@gmail.com>
2025-07-21 17:53:08 +0530

    route,redux,query setup

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,14 +1,26 @@
+import Body from "./components/Body";
 import LoginForm from "./components/LoginForm"
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import appStore from "./utlis/appStore";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 function App() {
+  const queryClient = new QueryClient()
 
   return (
     <>
-      <p className="flex justify-center text-3xl font-bold text-red-500 underline">
-        E-Commerce Application
-      </p>
-      <LoginForm />
+      <Provider store={appStore}>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter basename="/" >
+            <Routes>
+              <Route path="/" element={<Body />}>
+                <Route path="/" element={<LoginForm />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </QueryClientProvider>
+      </Provider>
     </>
   )
 }
